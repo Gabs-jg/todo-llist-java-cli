275bc0785e0e168cd3a6b9878623034b2d012785
Gabs-jg <gabriel19joao@gmail.com>
2026-08-29 23:54:09 -0300

    listAllTasks and getTaskById methods implemented

--- src/com/todolist/repository/TaskRepository.java
+++ src/com/todolist/repository/TaskRepository.java
@@ -1,9 +1,15 @@
 package com.todolist.repository;
 import com.todolist.model.Task;
 
+import java.util.List;
+
 public interface TaskRepository {
 
     void createTask(Task task);
+
+    List<Task> listAllTasks();
+
+    Task getTaskById(int id);
 
     void updateTask(Task task);
 
--- src/com/todolist/repository/TaskRepositoryImpl.java
+++ src/com/todolist/repository/TaskRepositoryImpl.java
@@ -15,6 +15,21 @@ public class TaskRepositoryImpl implements TaskRepository{
     }
 
     @Override
+    public List<Task> listAllTasks() {
+        return tasks;
+    }
+
+    @Override
+    public Task getTaskById(int id) {
+        for(Task task : tasks) {
+            if(task.getId() == id) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    @Override
     public void updateTask(Task task) {
         for(int i=0; i< tasks.size(); i++) {
             if(tasks.get(i).getId() == task.getId()) {
