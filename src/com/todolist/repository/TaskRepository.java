dcf8dcbe2966370ff2c92d0a539ec8e28db0bf09
Gabs-jg <gabriel19joao@gmail.com>
2026-08-29 23:15:43 -0300

    Implementation of the class TaskRepositoryImpl

--- src/com/todolist/repository/TaskRepository.java
+++ src/com/todolist/repository/TaskRepository.java
@@ -1,4 +1,5 @@
 package com.todolist.repository;
+import com.todolist.model.Task;
 
 public interface TaskRepository {
 
--- src/com/todolist/repository/TaskRepositoryImpl.java
+++ src/com/todolist/repository/TaskRepositoryImpl.java
@@ -1,4 +1,31 @@
 package com.todolist.repository;
+import com.todolist.model.Task;
 
-public class TaskRepositoryImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskRepositoryImpl implements TaskRepository{
+
+    List<Task> tasks = new ArrayList<>();
+
+
+    @Override
+    public void createTask(Task task) {
+        tasks.add(task);
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        for(int i=0; i< tasks.size(); i++) {
+            if(tasks.get(i).getId() == task.getId()) {
+                tasks.set(i, task);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void deleteTask(Task task) {
+        tasks.remove(task);
+    }
 }
