990971c17ab65d8d2d0538e398023cea731f8559
Gabs-jg <gabriel19joao@gmail.com>
2026-08-29 23:40:24 -0300

    Method createTask in TaskRepositoryImpl class implemented

--- src/com/todolist/service/TaskService.java
+++ src/com/todolist/service/TaskService.java
@@ -1,4 +1,28 @@
 package com.todolist.service;
 
+import com.todolist.model.Task;
+import com.todolist.repository.TaskRepository;
+
 public class TaskService {
+
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createTask(Task task) {
+        if(task.getNome() == null || task.getNome().isBlank()) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+
+        repository.createTask(task);
+    }
+
+    public void updateTask(Task task) {
+        // Regras de negócio
+    }
+
+    public void deleteTask(Task task) {
+    }
 }
